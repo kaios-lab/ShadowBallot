6ff6b8ba15fd8a9a533f068797c7c0d9907ca82e
kaios-lab <sbclyjg109@outlook.com>
2025-11-29 15:54:16 +0800

    Update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedShadowBallot = await deploy("ShadowBallot", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ShadowBallot contract: `, deployedShadowBallot.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_shadow_ballot"; // id required to prevent reexecution
+func.tags = ["ShadowBallot"];
